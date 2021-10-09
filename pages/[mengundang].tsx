f54920172f7fecb90e76bf88cfb5dd24c22b7e3f
ZR <zikriramdani.developer@gmail.com>
2021-10-09 11:12:12 +0700

    M

--- pages/[mengundang].tsx
+++ pages/[mengundang].tsx
@@ -34,11 +34,11 @@ const MengundangPage = () => {
         <link rel="icon" href="/images/flaticon/svg/003-luxury.svg" />
 
         {/* Facebook and Twitter integration */}
-        <meta property="site_name" content="Wedding Vivi & Zikri"/>
-        <meta property="title" content="Wedding Vivi & Zikri"/>
-        <meta property="description" content="Cipondoh, Kota Tangerang"/>
-        <meta property="image" content="https://firebasestorage.googleapis.com/v0/b/vue-js-271d9.appspot.com/o/testingarena%2Fno-image.jpg?alt=media"/>
-        <meta property="url" content="https://weddingvividanzikri.vercel.app"/>
+        <meta property="og:site_name" content="Wedding Vivi & Zikri"/>
+        <meta property="og:title" content="Wedding Vivi & Zikri"/>
+        <meta property="og:description" content="Cipondoh, Kota Tangerang"/>
+        <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/vue-js-271d9.appspot.com/o/testingarena%2Fno-image.jpg?alt=media"/>
+        <meta property="og:url" content="https://weddingvividanzikri.vercel.app"/>
 
         <meta name="twitter:title" content="Wedding Vivi & Zikri" />
         <meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/vue-js-271d9.appspot.com/o/testingarena%2Fno-image.jpg?alt=media" />
