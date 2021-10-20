c12d1473a8ce1ff84af15555c75f2a0ef0f74c19
ZR <zikriramdani.developer@gmail.com>
2021-10-21 05:34:47 +0700

    M

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps}) {
   useEffect(() => {
-    if("serviceWorker" in navigator) {
+    if ("serviceWorker" in navigator) {
       window.addEventListener("load", function () {
-       navigator.serviceWorker.register("/sw.js").then(
+        navigator.serviceWorker.register("/sw.js").then(
           function (registration) {
-            console.log("Service Worker registration successful with scope: ", registration.scope);
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
           },
           function (err) {
             console.log("Service Worker registration failed: ", err);
@@ -14,7 +17,7 @@ function MyApp({ Component, pageProps}) {
         );
       });
     }
-  }, [])
+  }, []);
   
   return <Component {...pageProps}/>
 }
