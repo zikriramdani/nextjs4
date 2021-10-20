cdea3afd58ef6c88f009671cba41bdd7f005a08c
ZR <zikriramdani.developer@gmail.com>
2021-10-21 04:22:18 +0700

    M

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-const webpack = require('webpack')
+const webpack = require('webpack');
 const withPWA = require("next-pwa");
+const securityHeaders = []
 
 module.exports = {
   reactStrictMode: true,
@@ -11,18 +12,27 @@ module.exports = {
         })
     )
     return config
-  }
+  },
   // eslint: {
   //   // Warning: This allows production builds to successfully complete even if
   //   // your project has ESLint errors.
   //   ignoreDuringBuilds: true,
   // },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
 
 module.exports = withPWA({
   pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
   },
+  reactStrictMode: true,
 });
