a2e21e10efbf8f7eb5d08a9a7af56cea71dc922d
ZR <zikriramdani.developer@gmail.com>
2021-10-21 10:34:20 +0700

    M

--- pages/[mengundang].jsx
+++ pages/[mengundang].jsx
@@ -34,6 +34,28 @@ const MengundangPage = () => {
       const manifestString = JSON.stringify({
         ...manifest,
         start_url: currentUrl,
+        "icons": [
+          {
+            "src": domain + "/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": domain + "/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": domain + "/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": domain + "/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
       });
       console.log('asd', currentUrl)
       manifestElement?.setAttribute(
