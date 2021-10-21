fcf8c5420154a3b665bfb0ff9eef8371097f489a
ZR <zikriramdani.developer@gmail.com>
2021-10-21 10:26:57 +0700

    M

--- pages/[mengundang].jsx
+++ pages/[mengundang].jsx
@@ -25,23 +25,21 @@ const MengundangPage = () => {
   const router = useRouter();
   // const query = router.query = queryString.parse(router.asPath.split(/\?/)[1]); // Destructuring our router object
 
-  // useEffect(() => {
-  //   if (router) {
-  //     var domain = document.location.origin;
-  //     var currentUrl = window. location. href;
-
-  //     const manifestElement = document.getElementById("manifest");
-  //     const manifestString = JSON.stringify({
-  //       ...manifest,
-  //       start_url: currentUrl,
-  //     });
-  //     console.log('asd', currentUrl)
-  //     manifestElement?.setAttribute(
-  //       "href",
-  //       "data:application/json;charset=utf-8," + encodeURIComponent(manifestString)
-  //     );
-  //   }
-  // }, [router]);
+  useEffect(() => {
+    var domain = document.location.origin;
+    var currentUrl = window. location. href;
+    
+    const manifestElement = document.getElementById("manifest");
+    const manifestString = JSON.stringify({
+      ...manifest,
+      start_url: currentUrl,
+    });
+    console.log('asd', currentUrl)
+    manifestElement?.setAttribute(
+      "href",
+      "data:application/json;charset=utf-8," + encodeURIComponent(manifestString)
+    );
+  }, []);
   
   return (
     <div>
