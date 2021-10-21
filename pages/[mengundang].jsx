784377a49ca5f444402fa342cc4a9be478d160f4
ZR <zikriramdani.developer@gmail.com>
2021-10-21 07:54:07 +0700

    M

--- pages/[mengundang].jsx
+++ pages/[mengundang].jsx
@@ -32,9 +32,10 @@ const MengundangPage = () => {
       const manifestElement = document.getElementById("manifest");
       const manifestString = JSON.stringify({
         ...manifest,
-        scope: router.asPath,
-        start_url: router.asPath,
+        scope: '/',
+        start_url: '/',
       });
+      console.log('asdas', router)
       manifestElement?.setAttribute(
         "href",
         "data:application/json;charset=utf-8," + encodeURIComponent(manifestString),
