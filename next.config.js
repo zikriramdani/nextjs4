7e2095993a769bdcd281afde4d42c7a001b00a91
ZR <zikriramdani.developer@gmail.com>
2021-10-21 09:27:00 +0700

    M

--- next.config.js
+++ next.config.js
@@ -29,10 +29,10 @@ module.exports = {
   },
 }
 
-// module.exports = withPWA({
-//   pwa: {
-//     dest: 'public',
-//     disable: process.env.NODE_ENV === 'development',
-//   },
-//   reactStrictMode: true,
-// });
+module.exports = withPWA({
+  pwa: {
+    // dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
+  reactStrictMode: true,
+});
