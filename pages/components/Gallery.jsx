d3f8eaff54e4454dff44a3807570bcdf3eff0699
ZR <zikriramdani.developer@gmail.com>
2021-10-24 18:23:13 +0700

    M

--- pages/components/Gallery.jsx
+++ pages/components/Gallery.jsx
@@ -1,3 +1,5 @@
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
 const GalleryPage = () => {
     return (
         <div id="Gallery" className="tabcontent content-hidden-xs">
@@ -22,29 +24,60 @@ const GalleryPage = () => {
                         <div className="col-md-3 col-sm-6">
                             <div className="gallery animate-box">
                                 <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9740-min.png">
-                                    <img src="/images/vividanzikri/IMG_9740-min.png" className="img-responsive" alt="" />
+                                    <LazyLoadImage
+                                    effect="black-and-white"
+                                    alt=""
+                                    src="/images/vividanzikri/IMG_9740-min.png" // use normal <img> attributes as props
+                                    />
+                                    {/* <img src="/images/vividanzikri/IMG_9740-min.png" className="img-responsive" alt="" /> */}
                                 </a>
                             </div>
                             <div className="gallery animate-box">
                             {/* <a className="gallery-img image-popup" href="images/gallery-3.jpg"><img src="/images/gallery-3.jpg" className="img-responsive" alt="" /></a> */}
-                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9726-min.png"><img src="/images/vividanzikri/IMG_9726-min.png" className="img-responsive" alt="" /></a>
+                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9726-min.png">
+                                    <LazyLoadImage
+                                    effect="black-and-white"
+                                    alt=""
+                                    src="/images/vividanzikri/IMG_9726-min.png" // use normal <img> attributes as props
+                                    />
+                                    {/* <img src="/images/vividanzikri/IMG_9726-min.png" className="img-responsive" alt="" /> */}
+                                </a>
                             </div>
                             <div className="gallery animate-box">
                             {/* <a className="gallery-img image-popup" href="images/gallery-4.jpg"><img src="/images/gallery-4.jpg" className="img-responsive" alt="" /></a> */}
-                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9729-min.png"><img src="/images/vividanzikri/IMG_9729-min.png" className="img-responsive" alt="" /></a>
+                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9729-min.png">
+                                    <LazyLoadImage
+                                    effect="black-and-white"
+                                    alt=""
+                                    src="/images/vividanzikri/IMG_9729-min.png" // use normal <img> attributes as props
+                                    />
+                                    {/* <img src="/images/vividanzikri/IMG_9729-min.png" className="img-responsive" alt="" /> */}
+                                </a>
                             </div>
                         </div>
                         <div className="col-md-3 col-sm-6">
                             <div className="gallery animate-box">
                                 {/* <a className="gallery-img image-popup image-popup" href="images/gallery-1.jpg"><img src="/images/gallery-1.jpg" className="img-responsive" alt="" /></a> */}
                                 <a className="gallery-img image-popup image-popup" href="images/vividanzikri/IMG_9673-min.png">
+                                    <LazyLoadImage
+                                    effect="black-and-white"
+                                    alt=""
+                                    src="/images/vividanzikri/IMG_9673-min.png" // use normal <img> attributes as props
+                                    />
                                     <img src="/images/vividanzikri/IMG_9673-min.png" className="img-responsive" alt="" />
                                 </a>
                             </div>
                             
                             <div className="gallery animate-box">
                             {/* <a className="gallery-img image-popup" href="images/gallery-2.jpg"><img src="/images/gallery-2.jpg" className="img-responsive" alt="" /></a> */}
-                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9710-min.png"><img src="/images/vividanzikri/IMG_9710-min.png" className="img-responsive" alt="" /></a>
+                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9710-min.png">
+                                    <LazyLoadImage
+                                    effect="black-and-white"
+                                    alt=""
+                                    src="/images/vividanzikri/IMG_9710-min.png" // use normal <img> attributes as props
+                                    />
+                                    {/* <img src="/images/vividanzikri/IMG_9710-min.png" className="img-responsive" alt="" /> */}
+                                </a>
                             </div>
                             {/* <div className="gallery animate-box">
                                 <a className="gallery-img image-popup" href="images/gallery-5.jpg"><img src="/images/gallery-5.jpg" className="img-responsive" alt="" /></a>
@@ -54,11 +87,25 @@ const GalleryPage = () => {
                         <div className="col-md-3 col-sm-6">
                             <div className="gallery animate-box">
                             {/* <a className="gallery-img image-popup" href="images/gallery-7.jpg"><img src="/images/gallery-7.jpg" className="img-responsive" alt="" /></a> */}
-                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9724-min.png"><img src="/images/vividanzikri/IMG_9724-min.png" className="img-responsive" alt="" /></a>
+                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9724-min.png">
+                                    <LazyLoadImage
+                                    effect="black-and-white"
+                                    alt=""
+                                    src="/images/vividanzikri/IMG_9724-min.png" // use normal <img> attributes as props
+                                    />
+                                    {/* <img src="/images/vividanzikri/IMG_9724-min.png" className="img-responsive" alt="" /> */}
+                                </a>
                             </div>
                             <div className="gallery animate-box">
                             {/* <a className="gallery-img image-popup" href="images/gallery-6.jpg"><img src="/images/gallery-6.jpg" className="img-responsive" alt="" /></a> */}
-                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9705-min.png"><img src="/images/vividanzikri/IMG_9705-min.png" className="img-responsive" alt="" /></a>
+                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9705-min.png">
+                                    <LazyLoadImage
+                                    effect="black-and-white"
+                                    alt=""
+                                    src="/images/vividanzikri/IMG_9705-min.png" // use normal <img> attributes as props
+                                    />
+                                    {/* <img src="/images/vividanzikri/IMG_9705-min.png" className="img-responsive" alt="" /> */}
+                                </a>
                             </div>
                         </div>
                         <div className="col-md-3 col-sm-6">
@@ -68,11 +115,25 @@ const GalleryPage = () => {
                             </div> */}
                             <div className="gallery animate-box">
                                 {/* <a className="gallery-img image-popup" href="images/gallery-10.jpg"><img src="/images/gallery-10.jpg" className="img-responsive" alt="" /></a> */}
-                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9693-min.png"><img src="/images/vividanzikri/IMG_9693-min.png" className="img-responsive" alt="" /></a>
+                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9693-min.png">
+                                    <LazyLoadImage
+                                    effect="black-and-white"
+                                    alt=""
+                                    src="/images/vividanzikri/IMG_9693-min.png" // use normal <img> attributes as props
+                                    />
+                                    {/* <img src="/images/vividanzikri/IMG_9693-min.png" className="img-responsive" alt="" /> */}
+                                </a>
                             </div>
                             <div className="gallery animate-box">
                             {/* <a className="gallery-img image-popup" href="images/gallery-9.jpg"><img src="/images/gallery-9.jpg" className="img-responsive" alt="" /></a> */}
-                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9738-min.png"><img src="/images/vividanzikri/IMG_9738-min.png" className="img-responsive" alt="" /></a>
+                                <a className="gallery-img image-popup" href="images/vividanzikri/IMG_9738-min.png">
+                                    <LazyLoadImage
+                                    effect="black-and-white"
+                                    alt=""
+                                    src="/images/vividanzikri/IMG_9738-min.png" // use normal <img> attributes as props
+                                    />
+                                    {/* <img src="/images/vividanzikri/IMG_9738-min.png" className="img-responsive" alt="" /> */}
+                                </a>
                             </div>
                         </div>
                     </div>
