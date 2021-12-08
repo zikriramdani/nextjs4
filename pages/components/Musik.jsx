d9fd414b264c6d3f23aee3ed0807713d0c69ffcc
ZR <zikriramdani.developer@gmail.com>
2021-12-09 02:19:26 +0700

    play

--- pages/components/Musik.jsx
+++ pages/components/Musik.jsx
@@ -2,7 +2,7 @@ const Musik = () => {
     return (
         <div className="musik">
             <audio id="myAudio" loop autoPlay>
-                <source src="https://jadinikah.site/wp-content/uploads/2021/08/Pamungkas-To-The-Bone-Reff.mp3" type="audio/mp3" />
+                <source src="/audio/Pamungkas-To-The-Bone-Reff.mp3" type="audio/mp3" />
                 Your browser does not support the audio element.
             </audio>
             <button className="btn-musik paused"></button>
